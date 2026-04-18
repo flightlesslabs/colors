119ce793fe673afd7314e24fc30d2198305629e0
Siddhesh Mangela <sidthecool007@gmail.com>
2026-04-18 19:29:00 +0530

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
@@ -17,6 +19,9 @@ const config = {
     adapter: adapter({
       fallback: 'index.html',
     }),
+  },
+  paths: {
+    base: dev ? '' : '/colors', // repo name
   },
 };
 
