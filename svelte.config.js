c2f99677c7a3fcdc3a29a721fe42132517aa675c
Siddhesh Mangela <sidthecool007@gmail.com>
2026-04-18 19:31:57 +0530

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
-    // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
     runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
   },
   extensions: ['.svelte', '.svx'],
@@ -19,9 +18,9 @@ const config = {
     adapter: adapter({
       fallback: 'index.html',
     }),
-  },
-  paths: {
-    base: dev ? '' : '/colors', // repo name
+    paths: {
+      base: dev ? '' : '/colors', // 👈 MUST be inside kit
+    },
   },
 };
 
